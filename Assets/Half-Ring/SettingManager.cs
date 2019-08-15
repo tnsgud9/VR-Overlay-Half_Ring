5b5c4925ca1733f7a1e8cd37b0bce64e18bf3687
SoonHyung_Lee <tnsgud9@naver.com>
2019-08-16 05:29:42 +0900

    UI add & More overlay image add

---
+++ Assets/Half-Ring/SettingManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityRawInput;
+
+public class SettingManager : OverlayRing 
+{
+
+
+    public static bool RawInputService = true;
+    bool overlayToggle=true;
+
+    void Awake()
+    {
+        RawKeyInput.Start(RawInputService);
+        Screen.SetResolution(640, 480, false);
+    }
+
+    void Update()
+    {
+       // RawKeyInput.OnKeyUp += OverayActive(Space);
+        if (RawKeyInput.IsKeyDown(RawKey.Space)) OverayActive();
+    }
+
+    void OverayActive()
+    {
+        overlayToggle = !overlayToggle;
+        overlayOBJ.SetActive(overlayToggle);
+    }
+
+    private void OnApplicationQuit()
+    {
+        RawKeyInput.Start(false);
+        RawKeyInput.Stop();
+
+    }
+}
---
+++ Assets/Half-Ring/UI_Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+
+    public GameObject[] category;
+    public GameObject[] contents;
+
+    void Start()
+    {
+        ButtonNotSelect();
+        category[0].transform.GetChild(0).GetComponent<Text>().color=new Color32(0,157,224,255);
+        contents[0].SetActive(true);
+    }
+    public void SelectButton(int index)
+    {
+        ButtonNotSelect();
+        category[index].transform.GetChild(0).GetComponent<Text>().color = new Color32(0, 157, 224, 255);
+        contents[index].SetActive(true);
+
+    }
+
+    void ButtonNotSelect()
+    {
+        for (int i = 0; i < category.Length; i++)
+        {
+            category[i].transform.GetChild(0).GetComponent<Text>().color = new Color32(231, 231, 231,255);
+            contents[i].SetActive(false);
+        }
+    }
+}
